afc4c3196f27dc2187652581357ed145f2e65258
ARAHEEM <araheem.work@outlook.com>
2026-05-29 00:16:04 -0700

    Refactor resource group data source to resource block and update references in service plan and web app configurations

--- env/dev/import.tf
+++ env/dev/import.tf
@@ -1,10 +1,14 @@
 import {
-  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourcegroups/rg-05-28-2026a/providers/Microsoft.Web/sites/myapp-05282026a"
+  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-05-28-2026a/providers/Microsoft.Web/sites/myapp-05282026a"
   to = module.app_service.azurerm_linux_web_app.example
 }
 
 import {
-  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-05-28-2026a/providers/Microsoft.Web/serverfarms/asp-05282026a"
+  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-05-28-2026a/providers/Microsoft.Web/serverFarms/asp-05282026a"
   to = module.app_service.azurerm_service_plan.example
 }
 
+import {
+  id="/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-05-28-2026a"
+  to= module.app_service.azurerm_resource_group.example
+}
